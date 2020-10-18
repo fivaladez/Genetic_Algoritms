47d630e3f83ca8ba75aadbbec5ddcd4823b87814
ivaladez-09 <ivan_valadez_11@hotmail.com>
2020-10-17 22:07:49 -0500

    Completing Curve Adjust problem

--- genetic_algorithm_curve_adjust.py
+++ genetic_algorithm_curve_adjust.py
@@ -1,8 +1,5 @@
 """This file contains the logic to use and create a genetic algorithm"""
-import random
-import math
 import matplotlib.pyplot as plt
-import pandas as pd
 import numpy as np
 
 
@@ -16,12 +13,13 @@ class GeneticAlgorithm:
         self.POPULATION_SIZE = int(population_size) if population_size > 0 else abs(population_size)
         self.CHROMOSOME_SIZE = int(chromosome_size) if chromosome_size > 0 else abs(chromosome_size)
         self.SHAPE = (self.POPULATION_SIZE, self.CHROMOSOME_SIZE)
-        self.best_chromosome = dict()
+        self.best_chromosome = list()  # [chromosome, aptitude_function]
         self.weight = 25.5
+        self.aptitude_function_history = np.array([], dtype=np.float32)
 
     def get_aptitude_function(self, population):
         """"""
-        aptitude_function_population = np.array([])
+        aptitude_function_population = np.array([], dtype=np.float32)
         population_index = 0
 
         while population_index < self.POPULATION_SIZE:
@@ -35,12 +33,39 @@ class GeneticAlgorithm:
                 chromosome_index += 1
                 exponent -= 1
 
-            aptitude_function = self.Y - expression
+            aptitude_function = np.float32(self.Y - expression)
             aptitude_function_population = np.append(aptitude_function_population,
                                                      [aptitude_function])
             population_index += 1
 
         return aptitude_function_population
+
+    def get_equation_y(self, chromosome, arr_x):
+        """"""
+        constants = chromosome / self.weight  # [A ... G] - where A = A' / weight
+        arr_y = np.array([])
+        for x in arr_x:
+            exponent = self.CHROMOSOME_SIZE - 1  # x^6 ... x^0
+            expression = 0
+            while exponent >= 0:
+                # (A'/weight) * x^6 ... (G'/weight) * x^0
+                expression += constants[self.CHROMOSOME_SIZE - exponent - 1] * pow(x, exponent)
+                exponent -= 1
+            arr_y = np.append(arr_y, [expression])
+
+        return arr_y
+
+    def get_y(self, chromosome, x):
+        """"""
+        constants = chromosome / self.weight  # [A ... G] - where A = A' / weight
+        exponent = self.CHROMOSOME_SIZE - 1  # x^6 ... x^0
+        y = 0
+        while exponent >= 0:
+            # (A'/weight) * x^6 ... (G'/weight) * x^0
+            y += constants[self.CHROMOSOME_SIZE - exponent - 1] * pow(x, exponent)
+            exponent -= 1
+
+        return y
 
     def get_random_population(self):
         """"""
@@ -51,15 +76,66 @@ class GeneticAlgorithm:
     def get_next_generation(self, population):
         """"""
         aptitude_function = self.get_aptitude_function(population)
+        childes_population = np.empty((0, self.CHROMOSOME_SIZE), dtype=np.uint8)
+        iterations = self.POPULATION_SIZE // 2
 
-        for i in range(self.POPULATION_SIZE):
+        for i in range(iterations):
+            # Getting father and mother chromosomes
             father_chromosome = self.get_tournament_winner(population, aptitude_function)
             mother_chromosome = self.get_tournament_winner(population, aptitude_function)
+
+            # Reproduce parents to get childes
             son, daughter = self.reproduction(father_chromosome, mother_chromosome)
             print("Parents: {}, {}".format(father_chromosome, mother_chromosome))
             print("Childes: {}, {}\n".format(son, daughter))
 
-        return None, None
+            # Add childes as rows for the new population (new generation)
+            childes_population = np.append(childes_population, [son], axis=0)
+            childes_population = np.append(childes_population, [daughter], axis=0)
+
+        if (iterations * 2) < self.POPULATION_SIZE:  # Repeat one more time with only one child
+            # Getting father and mother chromosomes
+            father_chromosome = self.get_tournament_winner(population, aptitude_function)
+            mother_chromosome = self.get_tournament_winner(population, aptitude_function)
+
+            # Reproduce parents to get childes
+            son, daughter = self.reproduction(father_chromosome, mother_chromosome)
+            print("Parents: {}, {}".format(father_chromosome, mother_chromosome))
+            print("Childes: {}, {}\n".format(son, daughter))
+
+            # Add childes as rows for the new population (new generation)
+            childes_population = np.append(childes_population, [son], axis=0)
+
+        # Getting the new aptitude function from new population
+        aptitude_function = self.get_aptitude_function(childes_population)
+
+        # Saving and comparing against the best chromosome from history
+        # Example: best_chromosome = [chromosome, aptitude_function]
+        best_chromosome = self.get_best_from_population(childes_population, aptitude_function)
+        if self.best_chromosome:
+            if abs(self.best_chromosome[1]) > abs(best_chromosome[1]):
+                self.best_chromosome = best_chromosome
+        else:
+            self.best_chromosome = best_chromosome
+
+        # Save the best aptitude function from the current population
+        self.aptitude_function_history = np.append(self.aptitude_function_history,
+                                                   abs(best_chromosome[1]))
+
+        return childes_population, aptitude_function
+
+    def get_best_from_population(self, population, aptitude_function):
+        """"""
+        # Finding the best from the current population
+        index = 0
+        best_index = index
+
+        while index < self.POPULATION_SIZE:
+            if abs(aptitude_function[best_index]) > abs(aptitude_function[index]):
+                best_index = index
+            index += 1
+
+        return population[best_index], aptitude_function[best_index]
 
     def get_tournament_winner(self, population, aptitude_function):
         """"""
@@ -73,9 +149,7 @@ class GeneticAlgorithm:
         winner_index = -1
         for index in contenders_indexes:
             if winner_index != -1:
-                winner_difference = abs(0 - aptitude_function[winner_index])
-                current_difference = abs(0 - aptitude_function[index])
-                if winner_difference > current_difference:
+                if abs(aptitude_function[winner_index]) > abs(aptitude_function[index]):
                     winner_index = index
             else:
                 winner_index = index
@@ -89,12 +163,20 @@ class GeneticAlgorithm:
         cut_point = np.random.randint(0, 8 * (self.CHROMOSOME_SIZE - 1), dtype=np.uint8)
 
         if cut_point % 8 == 0:
+            # Getting cut index - [0 ... index ... -1]
             index = int((cut_point / 8) - 1)
+
+            # Adding the chunks - [father ... mother]
             son = father[:index + 1]
             son = np.append(son, mother[index + 1:])
+
+            # Adding the chunks - [mother ... father]
             daughter = mother[:index + 1]
             daughter = np.append(daughter, father[index + 1:])
-            return son, daughter
+
+            print("Index {}".format(index))
+            # print("Son {}, Daughter {}".format(son, daughter))
+
         else:
             # Getting indexes - [first_part ... cut_part ... second_part]
             first_index = np.uint8(cut_point // 8)
@@ -108,8 +190,13 @@ class GeneticAlgorithm:
             first_cut_index = np.uint8(cut_point % 8)
             second_cut_index = 8 - first_cut_index
 
-            print("Cut index {} - Values {}, {}".format(first_index + 1, father[first_index + 1], mother[first_index + 1]))
-            print("Bits {}, {}".format(first_cut_index, second_cut_index))
+            """ Example:
+             father_cut_point = 134, mother_cut_point = 203
+             first_cut_index = 2, second_cut_index = 6 -> Bits from a Byte(8 bits)
+             Son = (left first 2 bits from 134 => 128) + (right first 6 bits from 203 => 11)
+             Daughter = (left first 2 bits from 203 => 192) + (right first 6 bits from 134 => 6)
+             Son = (128 + 11) = 139, Daughter = (192 + 6) = 198
+            """
 
             # Getting the new cut part for son chromosome
             first_cut_part = np.uint8((father_cut_point >> second_cut_index) << second_cut_index)
@@ -121,10 +208,46 @@ class GeneticAlgorithm:
             second_cut_part = np.uint8((father_cut_point << first_cut_index) >> first_cut_index)
             daughter_cut_point = first_cut_part + second_cut_part
 
-            print(son_cut_point, daughter_cut_point)
+            # Adding the chunks [father ... cut point ... mother]
+            son = father[:first_index + 1]
+            son = np.append(son, [son_cut_point])
+            son = np.append(son, mother[second_index:])
 
-            return None, None
+            # Adding the chunks [mother ... cut point ... father]
+            daughter = mother[:first_index + 1]
+            daughter = np.append(daughter, [daughter_cut_point])
+            daughter = np.append(daughter, father[second_index:])
 
-    def graph(self, population, aptitude_function):
+            print("Cut index {} - Values {}, {}".format(first_index + 1, father[first_index + 1],
+                                                        mother[first_index + 1]))
+            print("Bits {}, {}".format(first_cut_index, second_cut_index))
+            print("Son {}, Daughter {}".format(son_cut_point, daughter_cut_point))
+
+        return son, daughter
+
+    def graph(self, population, aptitude_function, generation):
         """"""
-        pass
+        # Getting coordinates for plotting
+        best_chromosome = self.get_best_from_population(population, aptitude_function)
+        x = np.arange(-100, 100, dtype=np.int16)
+        y = self.get_equation_y(best_chromosome[0], x)
+
+        # Plotting
+        plt.figure(1)
+        plt.plot(x, y)
+        plt.grid(color="gray", linestyle="--", linewidth=1, alpha=.4)
+        plt.title("Best Chromosome {}".format(best_chromosome))
+        plt.show(block=False)
+
+        plt.figure(2)
+        plt.plot(np.arange(0, generation, dtype=np.uint16), self.aptitude_function_history)
+        plt.grid(color="gray", linestyle="--", linewidth=1, alpha=.4)
+        plt.title("Closest to 0: {}".format(self.aptitude_function_history[-1]))
+        plt.show(block=False)
+        plt.pause(1)
+
+        plt.figure(1)
+        plt.clf()
+
+        plt.figure(2)
+        plt.clf()
--- main_curve_adjust.py
+++ main_curve_adjust.py
@@ -1,12 +1,16 @@
-import numpy as np
 from genetic_algorithm_curve_adjust import GeneticAlgorithm
 
 if __name__ == '__main__':
-    # Fixed values
-    X, Y = 1.5, 174.57
+    # Fixed values 77.3781, 76.96017
+    X, Y = 1.5, 77.3781
     POPULATION_SIZE = 100
     CHROMOSOME_SIZE = 7
-    GENERATIONS = 1
+    GENERATIONS = 20
+    """
+    # A=1,    B=2,    C=6,     D=3.2,  E=0.5,  F=1,    G=7
+    # A'=25,  B'=51,  C'=153,  D'=81,  E'=12,  F'=25,  G'=178
+    # Where A' = A * 25.5
+    """
 
     # Getting random initial population
     curve_adjust = GeneticAlgorithm(X, Y, POPULATION_SIZE, CHROMOSOME_SIZE)
@@ -15,8 +19,10 @@ if __name__ == '__main__':
     # Getting generations
     for generation in range(1, GENERATIONS + 1):
         child_population, aptitude_function = curve_adjust.get_next_generation(parent_population)
-        # curve_adjust.graph(child_population, aptitude_function)
-        # parent_population = child_population
+        curve_adjust.graph(child_population, aptitude_function, generation)
+        parent_population = child_population
+        print("\n\t==================================================\n\n")
 
-    # Presenting best chromosome
+    print("Best from History {}".format(curve_adjust.best_chromosome))
+    print("Aptitude function History {}".format(curve_adjust.aptitude_function_history))
     input("Pause")
